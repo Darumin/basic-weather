4496a7b381484ea58c89e55ab06098312bb57dfa
Darumin <jandeo92@gmail.com>
2020-09-10 23:18:03 -0700

    handled multiple search results

--- app/basic_forms.py
+++ app/basic_forms.py
@@ -1,6 +1,6 @@
 def display_weather(obs) -> dict:
     info: dict = obs.to_dict()
-    conversion = {
+    conversion: dict = {
         'temperature': obs.weather.temperature(unit='fahrenheit'),
         'sunrise_time': obs.weather.sunrise_time(timeformat='date'),
         'sunset_time': obs.weather.sunset_time(timeformat='date'),
@@ -9,3 +9,17 @@ def display_weather(obs) -> dict:
 
     info.update(conversion)
     return info
+
+
+def format_multiples(regis, q):
+    list_all: list = regis.locations_for(q)
+    ret: list = list()
+
+    if len(list_all) == 1: return False
+
+    for entry in list_all:
+        temp = f"{entry.name}, {entry.country} @ <lon:{entry.lon:.2f},"
+        temp += f" lat:{entry.lat:.2f}> with ID: ({entry.id})"
+        ret.append(temp)
+
+    return ret
--- app/basic_views.py
+++ app/basic_views.py
@@ -1,10 +1,12 @@
+# TODO: the search resolves multiples, but doesn't search by ID
+
 from pyowm import OWM
 from pyowm.commons.exceptions import NotFoundError
 
 from flask import Flask, render_template, request
 
 from app.bw_secrets import API_KEY
-from app.basic_forms import display_weather
+from app.basic_forms import display_weather, format_multiples
 
 owm = OWM(API_KEY)
 app = Flask(__name__)
@@ -18,17 +20,24 @@ def index():
 @app.route('/search_city', methods=['POST'])
 def search_city():
     manager = owm.weather_manager()
-    location: str = request.form['loc']
+    registry = owm.city_id_registry()
+    search_q: str = request.form['loc']
 
-    if location == '':
+    if search_q == '':
         e = 'Field cannot be blank.'
         return render_template('index.html', error=e)
 
     try:
-        observation = manager.weather_at_place(location)
+        observation = manager.weather_at_place(search_q)
     except NotFoundError:
         e = 'Entry not in registry. Please check your input.'
         return render_template('index.html', error=e)
+
+    dupes = format_multiples(registry, search_q)
+
+    if dupes:
+        e = 'There are many cities with that name. Try the city, country that matches best.'
+        return render_template('index.html', multi=dupes, error=e)
 
     info = display_weather(observation)
     return render_template('weather.html', info=info)
