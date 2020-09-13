d5ee1607b950abba2276d6572159934f8ed736f7
Darumin <jandeo92@gmail.com>
2020-09-12 19:36:54 -0700

    more superficial tidying

--- app/basic_views.py
+++ app/basic_views.py
@@ -2,11 +2,10 @@
 
 from pyowm import OWM
 from pyowm.commons.exceptions import NotFoundError
-
 from flask import Flask, render_template, request
 
 from app.bw_secrets import API_KEY
-from app.basic_forms import display_weather, format_multiples
+from app import basic_forms as bf
 
 owm = OWM(API_KEY)
 app = Flask(__name__)
@@ -17,10 +16,9 @@ def index():
     return render_template('index.html')
 
 
+# the route handling the search query and form box
 @app.route('/search_city', methods=['POST'])
 def search_city():
-    manager = owm.weather_manager()
-    registry = owm.city_id_registry()
     search_q: str = request.form['loc']
 
     if search_q == '':
@@ -28,19 +26,23 @@ def search_city():
         return render_template('index.html', error=e)
 
     try:
+        manager = owm.weather_manager()
         observation = manager.weather_at_place(search_q)
     except NotFoundError:
         e = 'Entry not in registry. Please check your input.'
         return render_template('index.html', error=e)
 
-    dupes = format_multiples(registry, search_q)
+    registry = owm.city_id_registry()
+    multi = bf.format_multiples(registry, search_q)
 
-    if dupes:
+    # in case of multiple cities with the same name are found
+    if multi:
         e = 'There are many cities with that name. Try the city, country that matches best.'
-        return render_template('index.html', multi=dupes, error=e)
-
-    info = display_weather(observation)
-    return render_template('weather.html', info=info)
+        return render_template('index.html', multi=multi, error=e)
+    else:
+        # if single entry, then proceed with weather report
+        info = bf.display_weather(observation)
+        return render_template('weather.html', info=info)
 
 
 if __name__ == '__main__':
